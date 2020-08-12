3c96099acc5cc79ab92983730b36bae645dd478c
se <se@mt-r.ru>
2020-08-12 09:28:13 +0300

    example updated

--- OPCDAClientSync.h
+++ OPCDAClientSync.h
@@ -22,8 +22,8 @@ struct OPCITEMSTATE
 
 struct OPCITEMDEF
 {
-    /* [string] */ LPWSTR szAccessPath;
-    /* [string] */ LPWSTR szItemID;
+    LPWSTR szAccessPath;
+    LPWSTR szItemID;
     BOOL bActive;
     OPCHANDLE hClient;
     DWORD dwBlobSize;
@@ -46,49 +46,37 @@ MIDL_INTERFACE("39c13a4d-011e-11d0-9675-0020afd8adb3")
 IOPCServer : IUnknown
 {
     virtual HRESULT STDMETHODCALLTYPE AddGroup( 
-        /* [string][in] */ LPCWSTR szName,
-        /* [in] */ BOOL bActive,
-        /* [in] */ DWORD dwRequestedUpdateRate,
-        /* [in] */ OPCHANDLE hClientGroup,
+        LPCWSTR szName,
+        BOOL bActive,
+        DWORD dwRequestedUpdateRate,
+        OPCHANDLE hClientGroup,
         /* [in][unique] */ LONG *pTimeBias,
         /* [in][unique] */ FLOAT *pPercentDeadband,
-        /* [in] */ DWORD dwLCID,
+        DWORD dwLCID,
         /* [out] */ OPCHANDLE *phServerGroup,
         /* [out] */ DWORD *pRevisedUpdateRate,
-        /* [in] */ REFIID riid,
+        REFIID riid,
         /* [iid_is][out] */ LPUNKNOWN *ppUnk) = 0;
-
-    virtual HRESULT STDMETHODCALLTYPE GetErrorString() = 0;
-    virtual HRESULT STDMETHODCALLTYPE GetGroupByName() = 0;
-    virtual HRESULT STDMETHODCALLTYPE GetStatus() = 0;
-    virtual HRESULT STDMETHODCALLTYPE RemoveGroup() = 0;
-    virtual HRESULT STDMETHODCALLTYPE CreateGroupEnumerator() = 0;
 };
 
 MIDL_INTERFACE("39c13a50-011e-11d0-9675-0020afd8adb3")
-IOPCGroupStateMgt : IUnknown
-{
-    virtual HRESULT STDMETHODCALLTYPE GetState() = 0;
-    virtual HRESULT STDMETHODCALLTYPE SetState() = 0;
-    virtual HRESULT STDMETHODCALLTYPE SetName() = 0;
-    virtual HRESULT STDMETHODCALLTYPE CloneGroup() = 0;
-};
+IOPCGroupStateMgt : IUnknown {};
 
 MIDL_INTERFACE("39c13a52-011e-11d0-9675-0020afd8adb3")
 IOPCSyncIO : IUnknown
 {
     virtual HRESULT STDMETHODCALLTYPE Read( 
-        /* [in] */ OPCDATASOURCE dwSource,
-        /* [in] */ DWORD dwCount,
+        OPCDATASOURCE dwSource,
+        DWORD dwCount,
         /* [size_is][in] */ OPCHANDLE *phServer,
-        /* [size_is][size_is][out] */ OPCITEMSTATE **ppItemValues,
-        /* [size_is][size_is][out] */ HRESULT **ppErrors) = 0;
+        /* [size_is][out] */ OPCITEMSTATE **ppItemValues,
+        /* [size_is][out] */ HRESULT **ppErrors) = 0;
 
     virtual HRESULT STDMETHODCALLTYPE Write( 
-        /* [in] */ DWORD dwCount,
+        DWORD dwCount,
         /* [size_is][in] */ OPCHANDLE *phServer,
         /* [size_is][in] */ VARIANT *pItemValues,
-        /* [size_is][size_is][out] */ HRESULT **ppErrors) = 0;
+        /* [size_is][out] */ HRESULT **ppErrors) = 0;
 };
 
 MIDL_INTERFACE("39c13a54-011e-11d0-9675-0020afd8adb3")
@@ -97,15 +85,8 @@ IOPCItemMgt : IUnknown
     virtual HRESULT STDMETHODCALLTYPE AddItems( 
         /* [in] */ DWORD dwCount,
         /* [size_is][in] */ OPCITEMDEF *pItemArray,
-        /* [size_is][size_is][out] */ OPCITEMRESULT **ppAddResults,
-        /* [size_is][size_is][out] */ HRESULT **ppErrors) = 0;
-
-    virtual HRESULT STDMETHODCALLTYPE ValidateItems() = 0;
-    virtual HRESULT STDMETHODCALLTYPE RemoveItems() = 0;
-    virtual HRESULT STDMETHODCALLTYPE SetActiveState() = 0;
-    virtual HRESULT STDMETHODCALLTYPE SetClientHandles() = 0;
-    virtual HRESULT STDMETHODCALLTYPE SetDatatypes() = 0;
-    virtual HRESULT STDMETHODCALLTYPE CreateEnumerator() = 0;
+        /* [size_is][out] */ OPCITEMRESULT **ppAddResults,
+        /* [size_is][out] */ HRESULT **ppErrors) = 0;
 };
 
 class OPCDAClientSync
--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #include "OPCDAClientSync.h"
 #include <cassert>
 
+void test (void)
+{
+	OPCDAClientSync opc (L"Graybox.Simulator.1");
+
+	for (int i = 0; i < 100; ++i)
+	{
+		opc.WriteItem (L"storage.string.reg01", L"aaa");
+		Sleep (5);
+		std::wstring aaa = opc.ReadItem (L"storage.string.reg01");
+		assert (L"aaa" == aaa);
+
+
+		opc.WriteItem (L"storage.string.reg01", L"bbb");
+		Sleep (5);
+		std::wstring bbb = opc.ReadItem (L"storage.string.reg01");
+		assert (L"bbb" == bbb);
+	}
+
+}
+
 void main(void)
 {
 	OPCDAClientSync::Initialize();
-	{
-		OPCDAClientSync opc (L"Graybox.Simulator.1");
-
-		for (int i = 0; i < 100; ++i)
-		{
-			opc.WriteItem (L"storage.string.reg01", L"aaa");
-			Sleep (5);
-			std::wstring aaa = opc.ReadItem (L"storage.string.reg01");
-			assert (L"aaa" == aaa);
-
-
-			opc.WriteItem (L"storage.string.reg01", L"bbb");
-			Sleep (5);
-			std::wstring bbb = opc.ReadItem (L"storage.string.reg01");
-			assert (L"bbb" == bbb);
-		}
-	}
+	test ();
 	OPCDAClientSync::Uninitialize ();
 }
